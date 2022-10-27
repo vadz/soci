fcc46f713f90525918995459435a7690ae27fcd1
Vadim Zeitlin <vadim@zeitlins.org>
2022-10-27 23:34:03 +0200

    Install postgresql in ODBC installation script too
    This does no harm if it's already installed but will install it when
    running inside a container where this is not the case by default.

--- scripts/ci/install_odbc.sh
+++ scripts/ci/install_odbc.sh
@@ -8,7 +8,7 @@ source ${SOCI_SOURCE_DIR}/scripts/ci/common.sh
 run_apt install \
     tar bzip2 \
     unixodbc unixodbc-dev \
-    odbc-postgresql odbc-postgresql-dbgsym
+    odbc-postgresql odbc-postgresql-dbgsym postgresql
 
 # Use full path to the driver library to avoid errors like
 # [01000][unixODBC][Driver Manager]Can't open lib 'psqlodbca.so' : file not found
